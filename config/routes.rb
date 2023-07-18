6103ced6d3eb3e0b314490d82104810a8ca5d828
Raghavendra Sharma <raghavedra.sharma@priciousinfosystem.com>
2023-07-18 17:36:03 +0530

    5th commit for create Signup and signin page complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :welcomes
+
+
+  devise_for :welcomes , :controllers => {registrations: 'registrations'} do
+    get 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
