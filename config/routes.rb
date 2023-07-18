7142111772d82f0c3c810db8f95e3172d57577e3
Raghavendra Sharma <raghavedra.sharma@priciousinfosystem.com>
2023-07-18 18:50:06 +0530

    8th commit for Adding User Controller and there actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/create'
+  # get 'users/edit'
+  # get 'users/update'
+  # get 'users/destroy'
 
+  root "users#index"
+  resources :users
+   
 
   devise_for :welcomes , :controllers => {registrations: 'registrations'} do
     get 'devise/sessions#destroy'
