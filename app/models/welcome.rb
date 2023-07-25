9e224e2382350fa2bb17c9cfccba2d850ff2990f
Raghavendra Sharma <raghavedra.sharma@priciousinfosystem.com>
2023-07-25 12:15:14 +0530

    Add confirmable mail before sign up time

--- app/models/welcome.rb
+++ app/models/welcome.rb
@@ -3,7 +3,7 @@ class Welcome < ApplicationRecord
   enum role: {Employee: 0, Passenger: 1, Admin: 2}
 
   # Include default devise modules. Others available are:
-  #  :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable, :confirmable
 
 end
