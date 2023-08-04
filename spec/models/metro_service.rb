2ef56d75ba5816777d99c908a3f14c612a174577
Raghavendra Sharma <raghavedra.sharma@priciousinfosystem.com>
2023-08-04 11:30:48 +0530

    write Rspec for validation of MetroService Model

--- spec/models/metro_service.rb
+++ spec/models/metro_service.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe MetroService, type: :model do
-
   describe "validations" do
-     
+    it 'is valid with valid attributes' do
+      metro_service = MetroService.new(source: "Vijay Nagar Metro Station", destination: "Sukhliya Metro Station")
+      expect(metro_service).to be_valid
+    end
+
+    it 'is not valid without a source' do
+      metro_service = MetroService.new(destination: "Sukhliya Metro Station")
+      expect(metro_service).not_to be_valid
+      expect(metro_service.errors[:source]).to include("can't be blank")
+    end
+
+    it 'is not valid without a destination' do
+      metro_service = MetroService.new(source: "Vijay Nagar Metro Station")
+      expect(metro_service).not_to be_valid
+      expect(metro_service.errors[:destination]).to include("can't be blank")
+    end
   end
 end
-
