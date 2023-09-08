aafac7a1612a574473aadab0d25f590a9f173548
Raghavendra Sharma <sharmaraghavendra42@gmail.com>
2023-09-08 12:45:29 +0530

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,7 @@ Train.create!(name:"Indore Express Metro",train_number:201992,source:"Vijay Naga
 Train.create!(name:"Vande Indore Metro",train_number:234567,source:"Bapat Metro Station",destination:"Vijay Nagar Metro Station",metro_service_id:2)
 Train.create!(name:"Indore SF Metro",train_number:123399,source:"MR10 Metro Station",destination:"Bapat Metro Station",metro_service_id:3)
 Train.create!(name:"ARCTSL Metro",train_number:322344,source:"MR10 Metro Station",destination:"Bapat Metro Station",metro_service_id:3)
+
+#user data
+# User.create!(email:"r@gmail.com",name:"Rghavendra Sharma",contact_number:"8907654324",address:"e block", gender:"male",password:123456)
+# User.create!(email:"admin@gmail.com",name:"Admin",contact_number:"8907656781",role: "Admin",address:"Metro Service Department", gender:"other",password:123456)
